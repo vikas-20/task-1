4a1c3a38dcd791996a4c50884853ef12bb2c6832
vikas-20 <vsdeoli2@gmail.com>
2021-08-21 14:29:07 +0530

    initial commit

---
+++ js/script.js.js
@@ -0,0 +1,35 @@
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+    })
+
+
+
+
+
+let thumbnail = document.getElementsByClassName('thumbnail')
+
+let activeImages = document.getElementsByClassName('active')
+
+for (var i=0; i < thumbnail.length; i++){
+
+  thumbnail[i].addEventListener('mouseover', function(){
+    //console.log(activeImages)
+
+    document.getElementById('featured').src = this.src
+  })
+}
+
+
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+
+buttonLeft.addEventListener('click', function(){
+  document.getElementById('slider').scrollLeft -= 80
+})
+
+buttonRight.addEventListener('click', function(){
+  document.getElementById('slider').scrollLeft += 80
+})
